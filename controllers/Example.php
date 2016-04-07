47e4e8397e1cea026c79081554a5015f7fd82b8d
Fil <Fil>
2016-04-06 18:57:30 -0700

    Update the controller and added example in the view/content

--- controllers/Example.php
+++ controllers/Example.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 /**
- * Class Example 
+ * Class Example
  *
  *
  * @author fil josepn elman
@@ -18,29 +18,18 @@ class Example extends My_Controller {
   }
 
   /**
-  * This method is responsible for rendering the
-  * list of company to the browser.
+  *
   * @return none
   */
-  public function view_company ($option=array()) {
-    if (!$this->ion_auth->logged_in()) {
-      redirect('main', 'refresh');
-    } else {
-      $data = array (
-      'title'         => COMPANY_NAME.' - '.'Company List',
-      'heading'       => 'Company',
+  public function view () {
+    $data = array (
+      'title'         => 'Example of the title',
+      'heading'       => 'Example',
       'navigation'    => 'navigation',
-      'content'       => 'company'.D.'company_list',
-      'company_list'  => $this->company_model->get_company_list(),
-      'add_company'   => 'add_company',
-      'edit_company'  => 'edit_company'
-      );
-      # merge option and data
-      $data = array_merge($data, $option);
-      parent::render($data);
-    }
+      'content'       => 'example',
+    );
+    parent::render($data);
   }
-
 }
 
 ?>
---
+++ views/content/example.php
@@ -0,0 +1,6 @@
+<div class="row">
+  <div class="">
+    <h1><?php echo $heading; ?></h1>
+    <p>Example</p>
+  </div>
+</div>
